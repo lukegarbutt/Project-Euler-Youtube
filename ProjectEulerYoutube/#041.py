af39a5b5698b8f6e01a5397bfef78c7c16d76d13
Luke Garbutt <luke@getlocalmeasure.com>
2018-08-30 20:55:14 +0100

    41 done

--- ProjectEulerYoutube/#041.py
+++ ProjectEulerYoutube/#041.py
@@ -1,4 +1,5 @@
 # Pandigital prime
+import itertools
 
 def prime_check(n):
     if n % 2 == 0:
@@ -10,8 +11,16 @@ def prime_check(n):
 
 def main():
     biggest_so_far = 0
-    numbers_to_use = []
-    for digit in range(1,10):
-        numbers_to_use.append(digit)
+    for length_of_number in range(9, 1, -1):
+        numbers_to_use = []
+        for digit in range(length_of_number, 0, -1):
+            numbers_to_use.append(digit)
+        possibilities = list(itertools.permutations(numbers_to_use, length_of_number))
+        for possible in possibilities:
+            number = int(''.join(str(i) for i in possible))
+            if prime_check(number):
+                print(number)
+                quit()
 
 
+main()
