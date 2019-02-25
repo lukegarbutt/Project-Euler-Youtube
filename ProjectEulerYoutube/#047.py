c2e9cc5a17ca77f8f542a10ef0c8a12628afdf98
Luke Garbutt <luke@getlocalmeasure.com>
2019-02-25 17:59:31 +0000

    Added some more functions but think I've misunderstood the question so this needs re doing

--- ProjectEulerYoutube/#047.py
+++ ProjectEulerYoutube/#047.py
@@ -29,7 +29,46 @@ def find_prime_factorisation(n, prime_list=[2,3]):
     return(prime_factors)
 
 
-print(find_prime_factorisation(876878766))
+def group_factorisation(factors):
+    factors_dict = {}
+    for factor in factors:
+        if factor in factors_dict.keys():
+            factors_dict[factor] += 1
+        else:
+            factors_dict[factor] = 1
+    return(factors_dict)
 
 
+def main():
+    x = 1
+    while True:
+        prime_factors_1 = group_factorisation(find_prime_factorisation(x))
+        prime_factors_2 = group_factorisation(find_prime_factorisation(x+1))
+        prime_factors_3 = group_factorisation(find_prime_factorisation(x+2))
+        prime_factors_4 = group_factorisation(find_prime_factorisation(x+3))
+        failed = False
+        if not failed:
+            for key, value in prime_factors_1.items():
+                if key in prime_factors_2 and value == prime_factors_2[key]:
+                    failed = True
+                elif key in prime_factors_3 and value == prime_factors_3[key]:
+                    failed = True
+                elif key in prime_factors_4 and value == prime_factors_4[key]:
+                    failed = True
+        if not failed:
+            for key, value in prime_factors_2.items():
+                if key in prime_factors_3 and value == prime_factors_3[key]:
+                    failed = True
+                elif key in prime_factors_4 and value == prime_factors_4[key]:
+                    failed = True
+        if not failed:
+            for key, value in prime_factors_3.items():
+                if key in prime_factors_4 and value == prime_factors_4[key]:
+                    failed = True
+        if not failed:
+            print("success, our numbers are {}, {}, {} and {}. The factors were {}, {}, {} and {}".format(x, x+1, x+2, x+3, prime_factors_1, prime_factors_2, prime_factors_3, prime_factors_4))
+            break
+        x += 1
 
+
+main()
